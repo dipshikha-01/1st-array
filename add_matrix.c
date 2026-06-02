6c9ed18c47ef463e1bacc7fa10207035d8daa0f9
dipshikha-01 <dipshikhajana2006@gmail.com>
2026-06-03 01:13:26 +0530

    Add files via upload

---
+++ add_matrix.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int row , col ,i , j;
+    printf("enter the number of rows and columns:");
+    scanf("%d %d",&row , &col);
+    int a[row][col] , b[row][col] , sum[row][col];
+    printf("enter the elements of first matrix : \n");
+    for(i=0 ; i<row ; i++)
+    {
+        for(j=0 ; j<col ;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("enter the elements of second matrix : \n");
+     for(i=0 ; i<row ; i++)
+    {
+        for(j=0 ; j<col ;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+
+     for(i=0 ; i<row ; i++)
+    {
+        for(j=0 ; j<col ;j++)
+        {
+            sum[i][j]= a[i][j] + b[i][j];
+        }
+    }
+
+     for(i=0 ; i<row ; i++)
+    {
+        for(j=0 ; j<col ;j++)
+        {
+          printf("%d" , sum[i][j]);
+        }
+        printf("\n");
+    }
+    
+return 0;
+}
